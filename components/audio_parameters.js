926391edc049d8c985deeda7a8ae5a4ed6e16fd2
DD-moe <damianb4000@gmail.com>
2025-04-18 21:25:43 +0200

    data

--- components/audio_parameters.js
+++ components/audio_parameters.js
@@ -1,9 +1,16 @@
 function audioParem() {
-    var iframe = document.getElementById('pdf-frame');
-    if (iframe) {
-        iframe.src = `./CTCAE.pdf`;
-    } else {
-        console.error('Brak elementu iframe na stronie.');
+    const container = document.getElementById('container');
+
+    // Wyczyść kontener
+    container.innerHTML = '';
+
+    // Załaduj obrazy od 0 do 17
+    for (let nr = 0; nr <= 17; nr++) {
+        const img = document.createElement('img');
+        img.src = `./images/CTCAE-obrazy-${nr}.jpg`;
+        img.alt = `Obraz ${nr}`;
+        img.classList.add('page-image');
+        container.appendChild(img);
     }
 }
 
